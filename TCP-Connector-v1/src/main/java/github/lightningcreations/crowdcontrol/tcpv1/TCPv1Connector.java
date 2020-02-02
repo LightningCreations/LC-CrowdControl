e5ec0ae7296aa0309918fef35439bc8c6ed481d5
Connor Horman <chorman64@gmail.com>
2020-02-02 04:52:49 -0500

    Less :thonk:
    Why did CrowdControl decide to use Java's least favourite character to terminate Objects.
    Yay.

--- TCP-Connector-v1/src/main/java/github/lightningcreations/crowdcontrol/tcpv1/TCPv1Connector.java
+++ TCP-Connector-v1/src/main/java/github/lightningcreations/crowdcontrol/tcpv1/TCPv1Connector.java
@@ -1,19 +1,23 @@
 package github.lightningcreations.crowdcontrol.tcpv1;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import github.lightningcreations.crowdcontrol.CrowdControlConnector;
 import github.lightningcreations.crowdcontrol.CrowdControlRequest;
+import github.lightningcreations.crowdcontrol.CrowdControlRequestType;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Deque;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class TCPv1Connector implements CrowdControlConnector {
     private Map<String,CompletableFuture<Void>> effectsToSend = new TreeMap<>();
@@ -21,8 +25,21 @@ public class TCPv1Connector implements CrowdControlConnector {
     private Thread t;
     private final AtomicBoolean shutdown = new AtomicBoolean(false);
 
+    private class Pair{
+        CrowdControlRequest.Response res;
+        CompletableFuture<Void> v;
+
+        public Pair(CrowdControlRequest.Response res, CompletableFuture<Void> v) {
+            this.res = res;
+            this.v = v;
+        }
+    }
+
     private Deque<CrowdControlRequest> reqQueue = new ConcurrentLinkedDeque<>();
     private Deque<CompletableFuture<CrowdControlRequest>> pollQueue = new ConcurrentLinkedDeque<>();
+    private final List<Pair> resList = Collections.synchronizedList(new LinkedList<>());
+
+    private static final Gson gson = new GsonBuilder().create();
 
     private static final int port = (int)(long)Long.getLong("crowdcontrol.connector.tcp.port",58430);
     private static final InetAddress toConnect;
@@ -39,7 +56,35 @@ public class TCPv1Connector implements CrowdControlConnector {
     }
 
     private void run(){
-        // TODO Thonk
+        try {
+            BufferedReader r = new BufferedReader(new InputStreamReader(sock.getInputStream(), StandardCharsets.UTF_8));
+            Writer w = new OutputStreamWriter(sock.getOutputStream(), StandardCharsets.UTF_8);
+            while(!shutdown.get()){
+                synchronized(resList){
+                    for(Pair p:resList){
+                        w.write(gson.toJson(p.res));
+                        w.append((char) 0);
+                    }
+                }
+                StringBuilder builder = new StringBuilder();
+                char c;
+                while((c = (char)r.read())!=0)
+                    builder.append(c);
+                CrowdControlRequest req = gson.fromJson(builder.toString(),CrowdControlRequest.class);
+                if(req.getCode().equals("__DISCOVER")) {
+                    if (req.getType() == CrowdControlRequestType.Test) {
+                        String s = String.join(", ", effectsToSend.keySet());
+                        effectsToSend.values().forEach(f -> f.complete(null));
+                        w.write(gson.toJson(req.success(s)));
+                    }
+                }
+                else if(pollQueue.isEmpty())
+                    reqQueue.add(req);
+                else
+                    pollQueue.removeFirst().complete(req);
+            }
+            sock.close();
+        } catch (IOException ignored) {}
     }
 
     @Override
@@ -54,7 +99,10 @@ public class TCPv1Connector implements CrowdControlConnector {
 
     @Override
     public CompletableFuture<Void> sendResult(CrowdControlRequest.Response response) {
-        return null;
+        CompletableFuture<Void> ret = new CompletableFuture<>();
+        Pair p = new Pair(response,ret);
+        resList.add(p);
+        return ret;
     }
 
     @Override
